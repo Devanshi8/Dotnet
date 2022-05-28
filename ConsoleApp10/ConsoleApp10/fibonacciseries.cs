840d55214bd35226df5dee887de0f37f1672ece3
Devanshi <devanshijoshi321@gmail.com>
2022-05-28 09:34:48 +0530

    Changed Program

--- ConsoleApp10/ConsoleApp10/fibonacciseries.cs
+++ ConsoleApp10/ConsoleApp10/fibonacciseries.cs
@@ -6,10 +6,11 @@ namespace ConsoleApp10
 {
     class fibonacciseries
     {
+        //Fibonacci series 
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the number of elements : ");
-            int n = Convert.ToInt32(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine()); //5
             Console.WriteLine("Fibonacci Series : ");
             fibseries(n);
         }
@@ -17,16 +18,15 @@ namespace ConsoleApp10
         {
             int n1 = 0;
             int n2 = 1;
-            Console.WriteLine(n1);
-            Console.WriteLine(n2);
+            Console.Write(n1+"  "+n2+"  "); //0 //1
             int n3;
-            while(n>0)
+            while(n>0)//5>0 4>0 3>0 2>0 1>0 0>0(false condn)
             {
-                n3 = n1 + n2;
-                Console.Write(n3+"  ");
-                n1 = n2;
-                n2 = n3;
-                n--;
+                n3 = n1 + n2;//0+1=1 1+1=2 1+2=3 2+3=5 3+5=8
+                Console.Write(n3+"  ");//1 //2 //3 //5 //8
+                n1 = n2;//n1=1 n1=1 n1=2 n1=3 n1=5
+                n2 = n3;//n2=1 n2=2 n2=3 n2=5 n2=8
+                n--;//n=4 n=3 n=2 n=1 n=0
                 
             }
 
