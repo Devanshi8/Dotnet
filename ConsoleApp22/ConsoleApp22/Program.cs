af032151a99a49f21c453fe2af38e4d231d1268d
Devanshi <devanshijoshi321@gmail.com>
2022-06-03 18:05:29 +0530

    Exception Handling

--- ConsoleApp22/ConsoleApp22/Program.cs
+++ ConsoleApp22/ConsoleApp22/Program.cs
@@ -2,11 +2,21 @@
 
 namespace ConsoleApp22
 {
+        // Exception Handling
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try//harmful code
+            {
+                int a = 5, b = 0;
+                int c = a / b;
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Division By Zero Error");
+            }
         }
     }
 }
