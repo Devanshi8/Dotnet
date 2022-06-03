2ad6d9e4b8932e9776f897aa690d8777c2eb069e
Devanshi <devanshijoshi321@gmail.com>
2022-06-03 18:47:20 +0530

    Exception Handling

--- ConsoleApp22/ConsoleApp22/Program.cs
+++ ConsoleApp22/ConsoleApp22/Program.cs
@@ -5,17 +5,34 @@ namespace ConsoleApp22
         // Exception Handling
     class Program
     {
-        static void Main(string[] args)
+        static void Main2(string[] args)
         {
             try//harmful code
             {
                 int a = 5, b = 0;
-                int c = a / b;
+               // int c = a / b;
+                int[] arr = new int[2];
+                arr[5] = 9;
+
 
             }
-            catch(Exception ex)
+            catch (IndexOutOfRangeException ex)
             {
-                Console.WriteLine("Division By Zero Error");
+                Console.WriteLine("Index out---Some error occured");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Some error occured");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            finally
+            {
+                Console.WriteLine("I am coming from finally");
             }
         }
     }
