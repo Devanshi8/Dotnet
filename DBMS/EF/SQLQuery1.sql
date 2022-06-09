19237c407e99e453440f8cb530a28085bacf29c0
Devanshi <devanshijoshi321@gmail.com>
2022-06-09 10:25:22 +0530

    Practice Problem

--- DBMS/EF/SQLQuery1.sql
+++ DBMS/EF/SQLQuery1.sql
@@ -1,4 +1,11 @@
 create table tblsample(id int identity(1,1)primary key,text varchar(50));
 select * from tblsample;
-
+use SampleDB
+create table Employee(ID int identity(1,1)primary key,FirstName varchar(20),LastName varchar(20),Gender varchar(8));
+select * from Employee;
+insert into Employee values('Devanshi','Joshi','Female');
+insert into Employee values('Arpit','Joshi','Male');
+insert into Employee values('Daivik','Thanki','Male');
+insert into Employee values('Krupa','Modha','Female');
+insert into Employee values('Vedant','Kumar','Male');
 
--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -13,9 +13,62 @@ namespace WebApplication1.Controllers
     public class HomeController : ControllerBase
     {
         SampleDBContext db = new SampleDBContext();
+
+        //Get Method
+        [HttpGet]
         public List<Tblsample> Get()
         {
             return db.Tblsamples.ToList();
         }
+
+        //insert record from body
+        [HttpPost]
+        public string Post([FromBody] string sample)
+        {
+            Tblsample t = new Tblsample();
+            t.Text = sample;
+            db.Tblsamples.Add(t);
+            db.SaveChanges();
+            return "Success";
+        }
+
+        //Update record 
+        [HttpPut]
+        public string Put([FromBody] Tblsample tblsample)
+        {
+            var tblSampleObj = db.Tblsamples.Where(x => x.Id == tblsample.Id);
+            if (tblSampleObj != null)
+            {
+                db.Tblsamples.Update(tblsample);
+                db.SaveChanges();
+                return "Success";
+            }
+            return "Fail";
+        }
+        //Delete Record
+        [HttpDelete]
+        public string Delete([FromBody] int Id)
+        {
+            var tblSampleObj = db.Tblsamples.Where(x => x.Id == Id).FirstOrDefault();
+            if (tblSampleObj != null)
+            {
+                db.Tblsamples.Remove(tblSampleObj);
+                db.SaveChanges();
+                return "Success";
+            }
+            return "Fail";
+        }
+
+        //MyCode***//Delete the record using id
+
+        //[HttpPost]
+        //public string Post([FromBody] int del)
+        //{
+        //    Tblsample tb = new Tblsample();//table object
+        //    tb.Id = del;//table Id column
+        //    db.Tblsamples.Remove(tb);//delete the record from row 
+        //    db.SaveChanges();//save the changes
+        //    return "Success!!";
+        //}***//
     }
 }
