10e51fdb6b97d90506f8984e5fe695d60ca6df12
Devanshi <devanshijoshi321@gmail.com>
2022-06-10 18:25:25 +0530

    Save Changes

--- Angular-Labs/first-sample-project/src/app/app.component.ts
+++ Angular-Labs/first-sample-project/src/app/app.component.ts
@@ -8,6 +8,19 @@ import { Customer } from './app.model';
 })
 export class AppComponent {
   title = 'first-sample-project';
-  myname='Devanshi'
-  CustomerModel:Customer=new Customer();
+  myname = 'Devanshi'
+  CustomerModel: Customer = new Customer();
+  CustomerModels: Array<Customer> = new Array<Customer>();
+  AddCustomer() 
+  {
+    this.CustomerModels.push(this.CustomerModel);
+    this.CustomerModel=new Customer();
+  }
+  EditCustomer(input: Customer) {
+    this.CustomerModel = input;
+  }
+  DeleteCustomer(input: Customer) {
+    var del = this.CustomerModels.indexOf(input);
+    this.CustomerModels.splice(del,1);
+  }
 }
