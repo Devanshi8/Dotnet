13c846d5eeca177f80f300899c25ebd019a984a9
Devanshi <devanshijoshi321@gmail.com>
2022-06-14 10:46:23 +0530

    Save Changes and new itema added

---
+++ DBMS/SQLQuery1(customersupplier).sql
@@ -0,0 +1,2 @@
+Create table Customer(ID int identity(1,1) primary key, CustomerCode varchar(50),CustomerName varchar(50),CustomerAmount int);
+Create table Supplier(ID int identity(1,1) primary key, SupplierCode varchar(50),SupplierName varchar(50),SupplierAmount int);
